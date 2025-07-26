c28ee47ce2c45d445940a7285f67b2c60172d20a
shreya <shreyapnseja@gmail.com>
2025-07-26 15:32:29 +0530

    updated code

---
+++ script.js
@@ -0,0 +1,18 @@
+function goToPage2() {
+      window.location.href = "page2.html";
+    }
+
+    function goToQuiz() {
+      window.location.href = "quiz.html";
+    }
+	function goToAbout() {
+  window.location.href = "about-us.html";
+}
+  function goToApplication(){
+  window.location.href = "application.html";
+}
+ function goToHome() {
+      window.location.href = "page.html";
+    }
+
+  
